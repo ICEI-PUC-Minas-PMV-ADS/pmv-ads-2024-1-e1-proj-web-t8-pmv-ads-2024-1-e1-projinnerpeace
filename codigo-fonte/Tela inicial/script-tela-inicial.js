77ed870008b171fd60525c36d0a4afc55acc307e
ErasmoAlves <114961595+ErasmoAlves@users.noreply.github.com>
2024-06-19 16:23:29 -0300

    Update script-tela-inicial.js

--- codigo-fonte/Tela inicial/script-tela-inicial.js
+++ codigo-fonte/Tela inicial/script-tela-inicial.js
@@ -4,19 +4,22 @@ const closeBtn = document.querySelector('.close');
 const cancelBtn = document.getElementById('cancelBtn');
 const confirmBtn = document.getElementById('confirmBtn');
 
+// Abre o modal quando o botão de sair é clicado
 logoutBtn.addEventListener('click', () => {
   modal.style.display = 'block';
 });
 
+// Fecha o modal quando o usuário clicar no botão 'X'
 closeBtn.addEventListener('click', () => {
   modal.style.display = 'none';
 });
 
+// Fecha o modal quando o usuário clicar no botão 'Cancelar'
 cancelBtn.addEventListener('click', () => {
   modal.style.display = 'none';
 });
 
+// Lógica para confirmar a saída do aplicativo
 confirmBtn.addEventListener('click', () => {
-  // Ainda precisa colocar a lógica de saída do aplicativo aqui
-  // ex: window.close();
+  
 });
