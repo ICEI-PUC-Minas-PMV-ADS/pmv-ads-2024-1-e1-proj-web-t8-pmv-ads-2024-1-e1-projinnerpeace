45ea34a213d488acd2567a1cc520118f5eae7f2e
monicaelo <145774768+monicaelo@users.noreply.github.com>
2024-06-22 19:09:06 -0300

    atualizacoes

--- codigo-fonte/cadastro/js/scripts.js
+++ codigo-fonte/cadastro/js/scripts.js
@@ -1,3 +1,58 @@
 {
     // "liveServer.settings.port"5501
 }
+{
+    // "liveServer.settings.port"5501
+}
+
+function cadastrar() {
+ 
+    if (nome.value == "" || nome.value.length < 8) {
+      alert("Preencha o formulário corretamente!");
+      nome.focus();
+      return;
+    }
+    if (email.value == "") {
+      alert("Preencha o formulário corretamente!");
+      email.focus();
+      return;
+    }
+    if (telefone.value == "" || telefone.value.length < 11) {
+      alert("Preencha o formulário corretamente!");
+      telefone.focus();
+      return;
+    }
+  
+    if (Senha.value == "" || Senha.value.length < 6) {
+      alert("Preencha o formulário corretamente!");
+      Senha.focus();
+      return;
+    }
+    if (ConfirmeSenha.value == "") {
+      alert("Preencha o formulário corretamente!");
+      ConfirmeSenha.focus();
+      return;
+    }
+  
+    if (Senha.value != ConfirmeSenha.value) {
+      alert("As senhas não correspondem!");
+      ConfirmeSenha.focus();
+      return;
+    }
+  
+    let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]')
+  
+    listaUser.push(
+      {
+        nomeUser: nome.value,
+        emailUser: formulario.email.value,
+        telefoneUser: formulario.telefone.value,
+        SenhaUser: formulario.Senha.value,
+        ConfirmeSenhaUser: formulario.ConfirmeSenha.value
+      });
+  
+    localStorage.setItem("listaUser", JSON.stringify(listaUser))
+  
+    alert("Usuário cadastrado com sucesso!");
+    window.location.href = "http://127.0.0.1:5502/login.html"
+  }
